34c434bec2e0514ce9bf72fde0c3ad58ab3bb988
swapnilKeshettivar <35079027+swapnilKeshettivar@users.noreply.github.com>
2019-02-05 10:39:01 +0000

    Update oracledagV9.py

--- dags/oracledagV9.py
+++ dags/oracledagV9.py
@@ -14,72 +14,72 @@ from datetime import datetime, timedelta
 from orclbqfilesV9.orclbm import *
 from orclbqfilesV9 import orclbm
 
-rightnow = datetime.combine(
-        datetime.today() - timedelta(7), datetime.min.time())
-schedule = timedelta(minutes=5)
+
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': rightnow,
+    'start_date': datetime(2019,1,31),
     'email': ['airflow@airflow.com'],
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retries': 5,
+    'retry_delay': timedelta(minutes=1),
+    #'schedule_interval': '@once',
+
 }
 
 
 dag = DAG(
- dag_id='orclbqbeambatchv9_1',
- schedule_interval=schedule, 
+ dag_id='Fact_GL_JE_LINES_7',
+ schedule_interval='@once',
  default_args=default_args
 )
 
-
 ddldict={"--connect_string": "159.65.85.83:1521/VIS",
-            "--username":"GL",
-            "--passwd":"GL",
-            "--separate_files":True,
-            "--forcedir":True,
-            "--datedir":False,
-            "--out":"file.out",
-            "--addverinfo":False,
-            "--alterbq":True,
-            "--gcred":"/home/abhishek/airflow/dags/BeamProjectV1-48b0a434a29a.json",
-            "--tables_only":True,
-            "--gcp": "beamprojectv1",
-            "--dtst": "AWSRDS_GL",
-            "--tname": "GL_JE_LINES"}
-bashargs = str(ddldict).replace(":", "").replace(",", "").replace("}", "").replace("{", "").replace("'","")
+          "--username": "GL",
+          "--passwd": "GL",
+          "--separate_files": True,
+          "--forcedir": True,
+          "--date_dir": False,
+          "--out": "file.out",
+          "--addverinfo": False,
+          "--alterbq": True,
+          "--gcred": "/home/swapnil_keshetty/airflow/dags/setupcrf-6da795b61160.json",
+          "--tables_only": True,
+          "--gcp": "setupcrf",
+          "--dtst": "AWSRDS_GL",
+          "--tname": "GL_JE_LINES"}
+bashargs = str(ddldict).replace(": ", " ").replace(",", "").replace("}", "").replace("{", "").replace("'","")
 run_DDL = BashOperator(
     task_id='createtabledefn',
-    bash_command="python /home/abhishek/airflow/dags/orclbqfilesV9/oracleddl.py "+bashargs,
+    bash_command="python /home/swapnil_keshetty/airflow/dags/orclbqfilesV9/oracleddl.py "+bashargs,
     dag=dag,
 )
-
-
 run_DT = PythonOperator(
     task_id='gettabledata',
     python_callable=orclbm.runbeampipe,
-    op_kwargs={"orclcn": "GL/GL@orcl.c7y14itdrmil.eu-west-1.rds.amazonaws.com:1521/ORCL",
-            "bqpid":"beamprojectv1",
+    op_kwargs={"orclcn": "apps/apps@159.65.85.83:1521/VIS",
+            "bqpid":"setupcrf",
             "bqds":"AWSRDS_GL",
             "bqtbl":"GL_JE_LINES",
-            "runner":"DirectRunner",
+            "runner":"DataflowRunner",
             "numwork":5,
             "autoscale":"THROUGHPUT_BASED",
             "maxnumwork":10,
             "setup":"./setup.py",
             "savesess":False,
             "gdfjob":"myjob",
-            "gcred":"/home/abhishek/airflow/dags/BeamProjectV1-48b0a434a29a.json",
-            "gbstaging":"gs://facbeambucketv1/staging",
-            "gbtemp":"gs://facbeambucketv1/temp"},
+            "gcred":"/home/swapnil_keshetty/airflow/dags/setupcrf-6da795b61160.json",
+            "gbstaging":"gs://crfbeamstoragebucket/staging",
+            "gbtemp":"gs://tempstoragebucket/temp"},
     dag=dag,
 )
 
 
 run_DDL >> run_DT
+
+
 
 
 if __name__ == "__main__":
